eb2481ab1c266d4dba4f57eeaaf9b8ce62649651
Gabriel Barros Aragão Silva <gabrielbarros@alu.ufc.br>
2021-03-25 09:09:44 -0300

    Atualizações no css

--- src/componentes/TelasCadastro/UsuarioAdm.jsx
+++ src/componentes/TelasCadastro/UsuarioAdm.jsx
@@ -6,19 +6,48 @@ export default class UsuarioAdm extends React.Component{
     siderData = [
         {
             title: 'Listar Usuários',
-            link: '/index'
+            link: '/index',
+            icone: 'BsListUl'
         },
         {
             title: 'Buscar Usuário',
-            link: '/buscar'
+            link: '/buscar',
+            icone: 'BsSearch'
         },
         {
             title: 'Criar Usuário',
-            link: '/criar'
+            link: '/criar',
+            icone: 'BsPlusCircle'
         },
         {
             title: 'Apagar Usuário',
-            link: '/apagar'
+            link: '/apagar',
+            icone: 'BsXCircle'
+        },
+        {
+            title: 'Atualizar Usuário',
+            link: '/atualizar',
+            icone: 'BsXCircle'
+        },
+        {
+            title: 'Sair',
+            link: '/',
+            icone: 'BsXCircle'
+        },
+        {
+            title: 'Apagar Usuário',
+            link: '/apagar',
+            icone: 'BsXCircle'
+        },
+        {
+            title: 'Atualizar Usuário',
+            link: '/atualizar',
+            icone: 'BsXCircle'
+        },
+        {
+            title: 'Sair',
+            link: '/',
+            icone: 'BsXCircle'
         },
     ]
     render(){
--- src/componentes/sidebar/Sidebar.jsx
+++ src/componentes/sidebar/Sidebar.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
 import './estilos.css'
 import { BiSortDown } from "react-icons/bi";
+import {BsListUl,BsSearch,BsPlusCircle,BsXCircle} from "react-icons/bs";
 
 export default class Sidebar extends React.Component{
     constructor(props){
         super(props)
     }
 
+    renderIcone(obj){
+        switch (obj) {
+            case 'BsListUl':
+                return <BsListUl className='icon'></BsListUl>
+            case 'BsSearch':
+                return <BsSearch className='icon'></BsSearch>
+            case 'BsPlusCircle':
+                return <BsPlusCircle className='icon'></BsPlusCircle>
+            case 'BsXCircle':
+                return <BsXCircle className='icon'></BsXCircle>
+        }
+    }
+
     renderButton(){
         const list = this.props.data.map(obj =>(
-            <button key={obj.title} id='bt'>{obj.title}</button>
+            <div id='conteudoList'>
+                <button key={obj.title} id='bt'>{obj.title}</button>
+                {this.renderIcone(obj.icone)}
+            </div>
+        ));
+        return list
+    }
+
+    renderContent(){
+        const list = this.props.data.map(obj =>(
+            <div id='itemContent'>
+                <h1>{obj.title}</h1>
+                <h2>{obj.link}</h2>
+                <h2>{obj.icone}</h2>
+            </div>
         ));
         return list
     }
@@ -17,19 +45,15 @@ export default class Sidebar extends React.Component{
     render(){
         return(
             <div id='containerReturn'>
-
-                <header id='header'>USUÁRIO ADMNISTRADOR</header>
-
-                <div id='conteudo'>CONTEUDO</div>
-                
-                <div id='principal'>
-                    <div id='SidebarEsq' class="bar">
-                        <div id='icone'>
-                            <BiSortDown className='icon' ></BiSortDown>
-                        </div>
-                        <div id='list'>
+                <div className='bar' id='SidebarEsq'>
+                    <div className='List'>    
                         {this.renderButton()}
-                        </div>
+                    </div>
+                </div>
+                <div id='page'>
+                    <div id='cabecalho'>Header</div>
+                    <div id='conteudo'>
+                        {this.renderContent()}
                     </div>
                 </div>
             </div>
